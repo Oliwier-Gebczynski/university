21a06934df65697e4370b74bfad6db4cdb0eb0ab
Oliwier-Gebczynski <oliwiergebczynski@gmail.com>
2022-12-27 23:50:48 +0100

    pierwszy etap zapisywania trasy + zapisanie to do

--- c++/ppk/projekt-semestr1/main.cpp
+++ c++/ppk/projekt-semestr1/main.cpp
@@ -1,3 +1,9 @@
+//TODO
+//- dokonczyc tworzenie sciezki
+//- ogarnac plik .h
+//- zrobic wlaczenie z terminalu -i [plik wejsciowy] -o [plik wyjsciowy] -s [nazwa miasta startowego]
+//- doxygen
+
 #include <iostream>
 #include <map>
 #include <set>
@@ -38,8 +44,11 @@ Graph LoadFromFile(const std::string& fileName) {
 }
 
 std::map<std::string, double> Dijkstra(const Graph& graph, std::string node) {                          //deklarujemy funkcje, node to punkt poczatkowy
-    std::map<std::string, double> result;                                                               //będziemy zwracać potem result, który bedzie mapą
+    std::map<std::string, double > result;                                                               //będziemy zwracać potem result, który bedzie mapą
     std::set<std::string> nodes;
+    std::vector<std::vector<std::string>> paths;
+    paths.push_back(std::vector<std::string> {node});
+
     for (const auto& el : graph) {                                                                      //dla każdego elementu w grafie przypisuje wartośc inf
         result[ el.first ] = std::numeric_limits<double>::infinity();
     }
@@ -50,10 +59,23 @@ std::map<std::string, double> Dijkstra(const Graph& graph, std::string node) {  
             if (result[el.first] > result[node] + el.second) {
                 result[el.first] = result[node] + el.second;
                 nodes.insert(el.first);
+
+                for (auto& item : paths) {   // blad z wychodzeniem poza zakres tablicy, jesli dodam do vectora to zwiekszy sie wielkosc i wykona sie jeszcze raz mimo ze nie ma tego elementu. Mozna sproboawac cos z nowa funkcja
+                    if (item.back() == node){
+                        std::vector<std::string> path;
+                        for(auto& element : item){
+                            path.push_back(element);
+                        }
+                        path.push_back(el.first);
+                        paths.push_back(path);
+                    }
+                }
+
             }
         }
+
         if (nodes.empty()) break;
-        node = *std::min_element(nodes.begin(), nodes.end(), [&result](const auto& l, const auto& r) {return result[l] < result[r]; });
+        node = *std::min_element(nodes.begin(), nodes.end(), [&result](const auto& l, const auto& r) {return result[l] < result[r]; });  //zmienia node na miasto z najmiejsza iloscia kilometrow do niego
         nodes.erase(node);
     }
     return result;
@@ -61,5 +83,5 @@ std::map<std::string, double> Dijkstra(const Graph& graph, std::string node) {  
 
 int main(){
     auto graph = LoadFromFile("lista.txt");
-    auto result = Dijkstra(graph, "Poznan");
+    auto result = Dijkstra(graph, "Katowice");
 }
