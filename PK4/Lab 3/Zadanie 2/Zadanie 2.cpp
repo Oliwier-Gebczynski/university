4baeee9d757458c619b02042f043bd4710e53623
Oliwier-Gebczynski <oliwiergebczynski@gmail.com>
2024-04-02 14:19:14 +0100

    Zadanie 2

---
+++ PK4/Lab 3/Zadanie 2/Zadanie 2.cpp
@@ -0,0 +1,19 @@
+﻿#include <filesystem>
+#include <fstream>
+#include <iostream>
+
+int main() {
+    std::filesystem::path path = "output.txt";
+    std::ofstream output_file(path);
+
+    if (output_file.is_open()) {
+        output_file << "Witaj, świecie!" << std::endl;
+        output_file.close();
+        std::cout << "File created and written successfully!" << std::endl;
+    }
+    else {
+        std::cerr << "Error opening file: " << path << std::endl;
+    }
+
+    return 0;
+}
