9d29e3c1d162310831085ede727b125394abc0d9
Oliwier-Gebczynski <oliwiergebczynski@gmail.com>
2024-04-02 14:08:42 +0100

    Zadanie 1

---
+++ PK4/Lab 3/Zadanie 1/Zadanie 1.cpp
@@ -0,0 +1,13 @@
+﻿#include <iostream>
+#include <filesystem>
+
+int main() {
+    std::filesystem::path path = "data.txt";
+    if (std::filesystem::exists(path)) {
+        std::cout << "Plik istnieje!" << std::endl;
+    }
+    else {
+        std::cout << "Plik nie istnieje." << std::endl;
+    }
+    return 0;
+}
