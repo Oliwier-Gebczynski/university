12e8eb07b15ea28a07897feac82bce94379a301b
Oliwier-Gebczynski <oliwiergebczynski@gmail.com>
2024-04-02 14:23:06 +0100

    Zadanie 3

---
+++ PK4/Lab 3/Zadanie 3/Zadanie 3.cpp
@@ -0,0 +1,19 @@
+﻿#include <filesystem>
+#include <fstream>
+#include <iostream>
+
+int main() {
+	std::filesystem::path source_path = "data.txt";
+	std::filesystem::path destination_path = "copy.txt";
+
+	std::ifstream source_file(source_path);
+	std::ofstream destination_file(destination_path);
+
+	destination_file << source_file.rdbuf();
+	source_file.close();
+	destination_file.close();
+
+	std::cout << "Done!";
+
+	return 0;
+}
