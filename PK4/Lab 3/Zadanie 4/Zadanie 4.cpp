07fdfc06ffad3e53877cbcacdd7a40bb7540bb69
Oliwier-Gebczynski <oliwiergebczynski@gmail.com>
2024-04-02 14:28:20 +0100

    Zadanie 4

---
+++ PK4/Lab 3/Zadanie 4/Zadanie 4.cpp
@@ -0,0 +1,20 @@
+﻿#include <filesystem>
+#include <iostream>
+
+void list_directory(const std::filesystem::path& path) {
+    for (const auto& entry : std::filesystem::directory_iterator(path)) {
+        if (std::filesystem::is_directory(entry)) {
+            std::cout << "Katalog: " << entry.path() << std::endl;
+            list_directory(entry.path());
+        }
+        else {
+            std::cout << "Plik: " << entry.path() << std::endl;
+        }
+    }
+}
+
+int main() {
+    std::filesystem::path directory_path = "./";
+    list_directory(directory_path);
+    return 0;
+}
