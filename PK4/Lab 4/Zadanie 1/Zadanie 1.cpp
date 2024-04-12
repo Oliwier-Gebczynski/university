8ba365803eecf26eb8e7bbd102906899fc635f98
Oliwier-Gebczynski <oliwiergebczynski@gmail.com>
2024-04-12 12:42:31 +0100

    Zadanie 1

---
+++ PK4/Lab 4/Zadanie 1/Zadanie 1.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+#include <vector>
+#include <ranges>
+#include <algorithm>
+#include <random>
+#include <string>
+
+int main()
+{
+    std::vector<int> data = { 1,2,3,4,5,6 };
+    
+    std::cout << "Przed zmiana: ";
+
+    for (auto element : data) {
+        std::cout << element << ", ";
+    }
+
+    std::cout << std::endl;
+
+    
+    auto view = data | std::views::take(3) | std::views::reverse;
+    std::ranges::sort(view);
+
+    std::cout << "Po zmianie: ";
+
+    for (auto element : data) {
+        std::cout << element << ", ";
+    }
+
+    return 0;
+}
