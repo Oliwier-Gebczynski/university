4a9dc413440abfbc7827c87e3ea94ff411173874
Oliwier-Gebczynski <65087108+Oliwier-Gebczynski@users.noreply.github.com>
2024-04-14 18:57:19 +0100

    404. Sum of Left Leaves

---
+++ Leetcode/C++/SumofLeftLeaves.cpp
@@ -0,0 +1,33 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int sum = 0;
+    void dfs(TreeNode* node){
+        if(node->left != nullptr){
+            if(node->left->left == nullptr && node->left->right == nullptr){
+                sum += node->left->val;
+            }
+            dfs(node->left);
+        }
+
+        if(node->right != nullptr){
+            dfs(node->right);
+        }
+    }
+
+
+    int sumOfLeftLeaves(TreeNode* root) {
+        dfs(root);
+        return sum;
+    }
+};
