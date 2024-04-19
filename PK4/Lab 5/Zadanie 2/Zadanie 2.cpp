bb4618b7fafb8f44e12d13a76589c8c496878efe
Oliwier-Gebczynski <oliwiergebczynski@gmail.com>
2024-04-19 12:40:44 +0200

    Zadanie 2

---
+++ PK4/Lab 5/Zadanie 2/Zadanie 2.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+#include <future>
+#include <functional>
+
+int main() {
+    int a = 13;
+    int b = 1;
+
+    double c = 3.14;
+    double d = 1.61;
+
+    std::future<int> intSumFuture = std::async([](int a, int b) { return a + b; }, a, b);
+    int intSum = intSumFuture.get();
+
+    std::future<double> doubleSumFuture = std::async([](double a, double b) { return a + b; }, c, d);
+    double doubleSum = doubleSumFuture.get();
+
+    std::cout << "Integer sum: " << intSum << std::endl;
+    std::cout << "Double sum: " << doubleSum << std::endl;
+
+    return 0;
+}
+
