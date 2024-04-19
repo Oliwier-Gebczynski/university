755ec8b43056ca6ce2a0d868693c4395b6640bcc
Oliwier-Gebczynski <oliwiergebczynski@gmail.com>
2024-04-19 12:27:57 +0200

    Zadanie 1

---
+++ PK4/Lab 5/Zadanie 1/Zadanie 1.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+#include <thread>
+
+void printMessageAndCalculatePowers(const std::string& message, int number) {
+    std::cout << message << std::endl;
+
+    int result = number;
+
+    for (int i = 1; i < 5; i++) {
+        std::cout << number << "^" << i << " = " << result << std::endl;
+        result *= number;
+    }
+}
+
+int main() {
+    std::thread thread1(printMessageAndCalculatePowers, "Calculations performed by thread 1: ", 2);
+    thread1.join();
+
+    std::thread thread2(printMessageAndCalculatePowers, "Calculations performed by thread 2: ", 3);
+    thread2.join();
+    
+    return 0;
+}
